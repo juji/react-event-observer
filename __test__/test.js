453dfe842907697aa08829a4f7c151238d90366d
Tri Gunadi <jujiyangasli@gmail.com>
2017-06-08 02:50:59 +0700

    handle multi subsciber

--- __test__/test.js
+++ __test__/test.js
@@ -42,6 +42,23 @@ describe('publish',function(){
 		
 	});
 
+	it('should handle multiple subscribers',function(){
+
+		var t = 0;
+		return new Promise((res,rej)=>{
+			observer.subscribe('main2',(data)=>{
+				t = t+data;
+				if(t==2) res(t);
+			});
+			observer.subscribe('main2',(data)=>{
+				t = t+data;
+				if(t==2) res(t);
+			});
+			observer.publish('main2',1);	
+		}).then(data => expect(data).toBe(2));
+		
+	});
+
 });
 
 describe('respond',function(){
